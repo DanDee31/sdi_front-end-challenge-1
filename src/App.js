6ba7f8163fb2605f98cfe1f8669c7342257cdf27
Daniel Raymond Ribaya <danielribaya31@gmail.com>
2025-03-07 17:22:52 +0800

    sdi front end challenge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Authors from "./JSON Data/Authors.json";
+import News from "./JSON Data/News.json";
 
 function App() {
+
+  const authorMap = Authors.reduce((acc, author) => {
+    acc[author.id] = author.name;
+    return acc;
+  }, {});
+  
+  const updateNews = News.map((news) => ({
+    ...news,
+    name: authorMap[news.author_id] || "Unknown Author",
+  }));
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <article>
+        {updateNews.map(news => {
+          const date = new Date(news.created_at);
+          const formattedDate = date.toLocaleString('en-US', {
+            day: '2-digit', month: 'short'
+          }).split(' ');
+
+          return (
+            <div key={news.id} className="item">
+              <div style={{ position: "relative" }}>
+                <img src={news.image_url} alt={news.title}/>
+                <div className="date">
+                  <span className="day">{formattedDate[1].toUpperCase()}</span>
+                  <span className="month">{formattedDate[0]}</span>
+                </div>
+              </div>
+              <div className="content">
+                <p className="share">
+                  <img src="share.png" alt="icon" className="icon"/>
+                  SHARE
+                </p>
+                <hr/>
+                <p className="author">{news.name}</p>
+                <h2 className="title">{news.title}</h2>
+                <p className="body">{news.body}</p>
+                <p className="link">READ ARTICLE</p>
+              </div>
+            </div>
+          );
+        })}
+
+        <div className="pagination">
+          <a href="#">&laquo;</a>
+          <a href="#">1</a>
+          <a href="#">2</a>
+          <a href="#">...</a>
+          <a href="#">8</a>
+          <a href="#">9</a>
+          <a href="#">10</a>
+          <a className="active" href="#">11</a>
+          <a href="#">12</a>
+          <a href="#">13</a>
+          <a href="#">14</a>
+          <a href="#">&raquo;</a>
+        </div>
+      </article>
     </div>
   );
 }
